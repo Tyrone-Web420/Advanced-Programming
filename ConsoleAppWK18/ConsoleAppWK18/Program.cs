0e3517b70ea40852e65889ff3be1fc11549e496b
Tyrone-Web420 <59957567+Tyrone-Web420@users.noreply.github.com>
2020-01-30 10:45:01 +0000

    Commit 1

---
+++ ConsoleAppWK18/ConsoleAppWK18/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWK18
+{
+    class Program
+    {
+        private static void Main(string[] args)
+
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Debug.WriteLine("Message From Output Window");
+            Console.WriteLine("Program has finished. Press enter to quit.", Color.Aquamarine);
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+
+}
